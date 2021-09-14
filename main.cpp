ec624c8e038ecd2ca45e75c1b6ad1cf3cbeda9b1
htesmile <472896179@qq.com>
2021-09-14 18:02:06 +0800

    first try

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int> A;
+    A.push_back(1);
+    A.push_back(2);
+    cout<<"hello world";
+    system("pause");
+    return 0;
+}
